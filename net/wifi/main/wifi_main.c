82ed3a0b1ee81862b5dcfb5afb925c0704825cd1
nosuz <nosuzuki@postcard.st>
2021-12-11 05:43:09 +0900

    Isolated NVS init.

--- net/wifi/main/wifi_main.c
+++ net/wifi/main/wifi_main.c
@@ -10,8 +10,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
 
+#include "nvs.h"
 #include "wifi.h"
 #include "http_get.h"
 
@@ -19,14 +19,7 @@ static const char *TAG = "wifi_main";
 
 void app_main(void)
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    nvs_init();
 
     wifi_init();
 
