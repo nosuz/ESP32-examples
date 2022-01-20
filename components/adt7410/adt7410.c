36c1c4ab08175950f1be7b8ac3b8325151a97acd
nosuz <nosuzuki@postcard.st>
2022-01-21 06:00:32 +0900

    Fixed converting to temperature.

--- components/adt7410/adt7410.c
+++ components/adt7410/adt7410.c
@@ -24,12 +24,12 @@ float adt7410_read_temp(void)
 
     union
     {
-        uint16_t temp;
+        int16_t value;
         struct temp_struct
         {
             // Little Endian
-            uint8_t high;
             uint8_t low;
+            uint8_t high;
         } data;
 
     } i2c_data;
@@ -45,19 +45,17 @@ float adt7410_read_temp(void)
     i2c_master_write_byte(cmd, ADT7410_TEMP_MSB, ACK_EN);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (ADT7410_ADDR << 1) | I2C_MASTER_READ, ACK_EN);
-    i2c_master_read_byte(cmd, &i2c_data.data.low, I2C_MASTER_ACK);
     i2c_master_read_byte(cmd, &i2c_data.data.high, I2C_MASTER_NACK);
+    i2c_master_read_byte(cmd, &i2c_data.data.low, I2C_MASTER_ACK);
     i2c_master_stop(cmd);
     // Execute and return status, should return 0
     ret = i2c_master_cmd_begin(CONFIG_I2C_PORT, cmd, pdMS_TO_TICKS(1000));
-    printf("I2C result: %02X", ret);
+    printf("I2C result: %02X\n", ret);
     i2c_cmd_link_delete(cmd);
-
-    printf("%02X", i2c_data.data.low);
-    printf(" %02X >> ", i2c_data.data.high);
-    printf("%04X\n", i2c_data.temp);
-
-    temp = 0.0625 * (i2c_data.temp >> 3); // 13-bits
+    // printf("%02X%02X >> ", i2c_data.data.high, i2c_data.data.low);
+    // printf("%04X\n", i2c_data.value);
+    int16_t value = i2c_data.value >> 3;
+    temp = (float)value * 0.0625;
 
     return temp;
 }
