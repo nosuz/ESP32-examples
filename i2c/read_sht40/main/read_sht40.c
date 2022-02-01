334ab516570d656c3088263e92dd9fe1d3d6f50b
nosuz <nosuzuki@postcard.st>
2022-02-01 20:30:10 +0900

    Added SHT4x component.

--- i2c/read_sht40/main/read_sht40.c
+++ i2c/read_sht40/main/read_sht40.c
@@ -17,16 +17,14 @@ void app_main(void)
     ESP_ERROR_CHECK(sht4x_reset());
 
     uint32_t serial = 0;
-    // ESP_ERROR_CHECK(sht4x_read_id(&serial));
-    sht4x_read_id(&serial);
+    ESP_ERROR_CHECK(sht4x_read_id(&serial));
     printf("Serial: %08X\n", serial);
 
     while (1)
     {
         float temp, humid;
 
-        // ESP_ERROR_CHECK(sht4x_read_sensor(&temp, &humid));
-        sht4x_read_sensor(&temp, &humid);
+        ESP_ERROR_CHECK(sht4x_read_sensor(&temp, &humid));
         printf("Tm=%.1fC\n", temp);
         printf("Hm=%.1f%%\n", humid);
         vTaskDelay(pdMS_TO_TICKS(5 * 1000));
