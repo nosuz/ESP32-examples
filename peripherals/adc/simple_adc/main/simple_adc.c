d391f685bc39ff1d843770169a24b352032d9d84
nosuz <nosuzuki@postcard.st>
2022-02-03 20:56:31 +0900

    Make ADC as a component

--- peripherals/adc/simple_adc/main/simple_adc.c
+++ peripherals/adc/simple_adc/main/simple_adc.c
@@ -3,52 +3,10 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/adc_common.h"
-#include "esp_adc_cal.h"
 
-#define ADC1_CHANNEL CONFIG_ADC1_CHANNEL
+#include "ns_adc.h"
 
-//ADC Calibration
-#if CONFIG_IDF_TARGET_ESP32
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_VREF
-#elif CONFIG_IDF_TARGET_ESP32S2
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32C3
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32S3
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP_FIT
-#endif
-
-static const char *TAG = "adc";
-
-static esp_adc_cal_characteristics_t adc_chars;
-
-static bool adc_init(void)
-{
-    esp_err_t ret;
-    bool cali_enable = false;
-
-    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
-    if (ret == ESP_ERR_NOT_SUPPORTED)
-    {
-        ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
-    }
-    else if (ret == ESP_ERR_INVALID_VERSION)
-    {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    }
-    else if (ret == ESP_OK)
-    {
-        cali_enable = true;
-        esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc_chars);
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Invalid arg");
-    }
-
-    return cali_enable;
-}
+static const char *TAG = "main";
 
 void app_main(void)
 {
@@ -56,19 +14,15 @@ void app_main(void)
     int sum_size = 16;
 
     uint32_t voltage;
-    bool cali_enable = adc_init();
-
-    //ADC1 config
-    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
-    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL, ADC_ATTEN_DB_11));
+    float battery_voltage;
+    init_adc();
 
     ESP_LOGI(TAG, "Read ADC");
     while (1)
     {
-        adc_raw = adc1_get_raw(ADC1_CHANNEL);
-        if (cali_enable)
+        adc_raw = read_adc_raw();
+        if (read_adc_voltage(&voltage) == ESP_OK)
         {
-            voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc_chars);
             printf("cali data: %2d mV ", voltage);
         }
         else
@@ -80,13 +34,16 @@ void app_main(void)
         adc_raw = 0;
         for (int i = 0; i < sum_size; i++)
         {
-            adc_raw = adc_raw + adc1_get_raw(ADC1_CHANNEL);
+            adc_raw = adc_raw + read_adc_raw();
         }
         adc_raw = adc_raw / sum_size;
-        if (cali_enable)
+
+        if (calc_adc_voltage_available())
         {
-            voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc_chars);
+            voltage = calc_adc_voltage(adc_raw);
             printf("cali avr. data: %3d mV\n", voltage);
+            battery_voltage = (float)voltage * 3 / 1000;
+            printf("Battery: %0.2f V\n", battery_voltage);
         }
         else
         {
