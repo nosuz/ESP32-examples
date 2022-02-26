f1207c3c9b8ee9762d3ded79c69e892575c6d219
nosuz <nosuzuki@postcard.st>
2022-02-26 09:31:29 +0900

    User ESP_OK to check returned result.

--- components/ns_http/ns_http.c
+++ components/ns_http/ns_http.c
@@ -74,7 +74,7 @@ esp_err_t ns_http_event_handler(esp_http_client_event_t *evt)
         ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
         int mbedtls_err = 0;
         esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
-        if (err != 0)
+        if (err != ESP_OK)
         {
             ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
             ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
