e92b55a4a9966ce0e8b593b6046b736e8ffb4b70
nosuz <nosuzuki@postcard.st>
2022-04-23 10:29:16 +0900

    On ADT7410, made 16 bits resolution available.

--- components/ns_adt7410/include/ns_adt7410.h
+++ components/ns_adt7410/include/ns_adt7410.h
@@ -1,3 +1,6 @@
 #include "ns_i2c_master.h"
 
+esp_err_t adt7410_set_configure(uint8_t config);
+esp_err_t adt7410_set_high_resoluton(void);
+esp_err_t adt7410_set_low_resoluton(void);
 esp_err_t adt7410_read_temp(float *temp);
