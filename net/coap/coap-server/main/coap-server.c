effb34a3f737dee78cfabe387af999e849b3026d
nosuz <nosuzuki@postcard.st>
2022-04-24 15:09:08 +0900

    Added CoAP server sample.

--- net/coap/coap-server/main/coap-server.c
+++ net/coap/coap-server/main/coap-server.c
@@ -1,6 +1,234 @@
-#include <stdio.h>
+/*
+
+This code is based on the esp-idf example.
+https://github.com/espressif/esp-idf/tree/master/examples/protocols/coap_server
+
+*/
+
+// #include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "ns_wifi.h"
+#include "coap3/coap.h"
+
+#define COAP_LOG_LEVEL ESP_LOG_DEBUG
+
+const static char *TAG = "coap";
+
+static char espressif_data[100];
+static int espressif_data_len = 0;
+
+#define INITIAL_DATA "Hello World!"
+
+/*
+ * The resource handler
+ */
+static void
+hnd_espressif_get(coap_resource_t *resource,
+                  coap_session_t *session,
+                  const coap_pdu_t *request,
+                  const coap_string_t *query,
+                  coap_pdu_t *response)
+{
+    // if (query->length > 0)
+    //     ESP_LOGI(TAG, "GET: %s", query->s);
+    // else
+    ESP_LOGI(TAG, "GET");
+
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+    coap_add_data_large_response(resource, session, request, response,
+                                 query, COAP_MEDIATYPE_TEXT_PLAIN, 60, 0,
+                                 (size_t)espressif_data_len,
+                                 (const u_char *)espressif_data,
+                                 NULL, NULL);
+}
+
+static void
+hnd_espressif_put(coap_resource_t *resource,
+                  coap_session_t *session,
+                  const coap_pdu_t *request,
+                  const coap_string_t *query,
+                  coap_pdu_t *response)
+{
+    size_t size;
+    size_t offset;
+    size_t total;
+    const unsigned char *data;
+
+    ESP_LOGI(TAG, "PUT");
+    coap_resource_notify_observers(resource, NULL);
+
+    if (strcmp(espressif_data, INITIAL_DATA) == 0)
+    {
+        coap_pdu_set_code(response, COAP_RESPONSE_CODE_CREATED);
+    }
+    else
+    {
+        coap_pdu_set_code(response, COAP_RESPONSE_CODE_CHANGED);
+    }
+
+    /* coap_get_data_large() sets size to 0 on error */
+    (void)coap_get_data_large(request, &size, &data, &offset, &total);
+
+    if (size == 0)
+    { /* re-init */
+        snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+        espressif_data_len = strlen(espressif_data);
+    }
+    else
+    {
+        espressif_data_len = size > sizeof(espressif_data) ? sizeof(espressif_data) : size;
+        memcpy(espressif_data, data, espressif_data_len);
+    }
+}
+
+static void
+hnd_espressif_delete(coap_resource_t *resource,
+                     coap_session_t *session,
+                     const coap_pdu_t *request,
+                     const coap_string_t *query,
+                     coap_pdu_t *response)
+{
+    ESP_LOGI(TAG, "DELETE");
+    coap_resource_notify_observers(resource, NULL);
+    snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+    espressif_data_len = strlen(espressif_data);
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_DELETED);
+}
+
+static void
+coap_log_handler(coap_log_t level, const char *message)
+{
+    uint32_t esp_level = ESP_LOG_INFO;
+    char *cp = strchr(message, '\n');
+
+    if (cp)
+        ESP_LOG_LEVEL(esp_level, TAG, "%.*s", (int)(cp - message), message);
+    else
+        ESP_LOG_LEVEL(esp_level, TAG, "%s", message);
+}
+
+static void coap_example_server(void *p)
+{
+    coap_context_t *ctx = NULL;
+    coap_address_t serv_addr;
+    coap_resource_t *resource = NULL;
+
+    snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+    espressif_data_len = strlen(espressif_data);
+    coap_set_log_handler(coap_log_handler);
+    coap_set_log_level(COAP_LOG_LEVEL);
+
+    while (1)
+    {
+        coap_endpoint_t *ep = NULL;
+        unsigned wait_ms;
+
+        /* Prepare the CoAP server socket */
+        coap_address_init(&serv_addr);
+        serv_addr.addr.sin6.sin6_family = AF_INET6;
+        serv_addr.addr.sin6.sin6_port = htons(COAP_DEFAULT_PORT);
+
+        ctx = coap_new_context(NULL);
+        if (!ctx)
+        {
+            ESP_LOGE(TAG, "coap_new_context() failed");
+            continue;
+        }
+        coap_context_set_block_mode(ctx,
+                                    COAP_BLOCK_USE_LIBCOAP | COAP_BLOCK_SINGLE_BODY);
+
+        ep = coap_new_endpoint(ctx, &serv_addr, COAP_PROTO_UDP);
+        if (!ep)
+        {
+            ESP_LOGE(TAG, "udp: coap_new_endpoint() failed");
+            goto clean_up;
+        }
+        ep = coap_new_endpoint(ctx, &serv_addr, COAP_PROTO_TCP);
+        if (!ep)
+        {
+            ESP_LOGE(TAG, "tcp: coap_new_endpoint() failed");
+            goto clean_up;
+        }
+
+        // coap://esp32.loacl/test
+        resource = coap_resource_init(coap_make_str_const("test"), 0);
+        if (!resource)
+        {
+            ESP_LOGE(TAG, "coap_resource_init() failed");
+            goto clean_up;
+        }
+        coap_register_handler(resource, COAP_REQUEST_GET, hnd_espressif_get);
+        coap_register_handler(resource, COAP_REQUEST_PUT, hnd_espressif_put);
+        coap_register_handler(resource, COAP_REQUEST_DELETE, hnd_espressif_delete);
+        /* We possibly want to Observe the GETs */
+        coap_resource_set_get_observable(resource, 1);
+        coap_add_resource(ctx, resource);
+
+        // coap://esp32.loacl/sample
+        resource = coap_resource_init(coap_make_str_const("sample"), 0);
+        if (!resource)
+        {
+            ESP_LOGE(TAG, "coap_resource_init() failed");
+            goto clean_up;
+        }
+        coap_register_handler(resource, COAP_REQUEST_GET, hnd_espressif_get);
+        coap_add_resource(ctx, resource);
+
+        wait_ms = COAP_RESOURCE_CHECK_TIME * 1000;
+
+        while (1)
+        {
+            int result = coap_io_process(ctx, wait_ms);
+            if (result < 0)
+            {
+                break;
+            }
+            else if (result && (unsigned)result < wait_ms)
+            {
+                /* decrement if there is a result wait time returned */
+                wait_ms -= result;
+            }
+            if (result)
+            {
+                /* result must have been >= wait_ms, so reset wait_ms */
+                wait_ms = COAP_RESOURCE_CHECK_TIME * 1000;
+            }
+        }
+    }
+clean_up:
+    coap_free_context(ctx);
+    coap_cleanup();
+
+    vTaskDelete(NULL);
+}
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Init Wifi");
+    wifi_init();
+    ESP_LOGI(TAG, "Try to connect");
+    wifi_connect();
 
+    ESP_LOGI(TAG, "Wait connection");
+    if (wifi_wait_connection())
+    {
+        ESP_LOGI(TAG, "Connected");
+        xTaskCreate(coap_example_server, "coap", 8 * 1024, NULL, 5, NULL);
+
+        while (1)
+            vTaskDelay(pdMS_TO_TICKS(2 * 1000));
+
+        // wifi_disconnect();
+        // ESP_LOGI(TAG, "Disconnected");
+    }
+    else
+    {
+        ESP_LOGW(TAG, "Failed to connect");
+    }
 }
