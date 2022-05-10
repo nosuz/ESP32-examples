a01cd15212486bbd5a4c3fe9511e8babd635ab9c
nosuz <nosuzuki@postcard.st>
2022-05-10 21:59:01 +0900

    Config PurgeCSS in vite.config.js.
    Avoid PurgeCSS during development phase.

--- components/ns_wifi/ap-select-app/vite.config.js
+++ components/ns_wifi/ap-select-app/vite.config.js
@@ -1,27 +1,38 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import viteCompression from "vite-plugin-compression";
-
+import purgecss from '@fullhuman/postcss-purgecss'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
+export default defineConfig(({ command, mode }) => {
   // console.log(command, mode)
-  if (mode == "development") {
-    return {
-      plugins: [svelte()],
-      server: {
-        proxy: {
-          '/api':{
-            target: 'http://esp32.local:80/',
-            changeOrigin: true,
-            secure: false,
-          }
-        }
-      }
-    }
-  } else {
+  if (mode === "production") {
     return {
       plugins: [svelte(), viteCompression()],
-    }
+      css: {
+        postcss: {
+          plugins: [
+                purgecss({
+                  content: ['dist/*.html', 'dist/assets/*.js'],
+                  css: ['dist/assets/*.css'],
+                  safelist: [/filepond-*/],
+                })
+          ]
+        }
+      }
+    };
+  } else {
+    return {
+      plugins: [svelte()],
+        server: {
+          proxy: {
+            '/api':{
+              target: 'http://esp32.local:80/',
+              changeOrigin: true,
+              secure: false,
+            }
+          }
+        }
+    };
   }
-})
+});
