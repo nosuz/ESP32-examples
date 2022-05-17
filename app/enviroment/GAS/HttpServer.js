464b04bff1d74e2c12538594d3bdce91077db9d7
nosuz <nosuzuki@postcard.st>
2022-05-17 09:56:24 +0900

    Tweet enviroment status from GAS.

--- app/enviroment/GAS/HttpServer.js
+++ app/enviroment/GAS/HttpServer.js
@@ -6,7 +6,6 @@
 // client_secret: This string is required only when the client is registered as a private client.
 // debug: set 1 will records posted JSON data on the log sheet.
 
-
 function doPost(e) {
   const sheet = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId())
 
@@ -49,4 +48,150 @@ function doPost(e) {
   // output.setContent(JSON.stringify({status: "OK"}))
 
   // return output
+}
+
+function getTimestamp() {
+  const date = new Date()
+  return Math.floor(date.getTime()/1000)
+}
+
+function refreshToken() {
+  const url = "https://api.twitter.com/2/oauth2/token"
+
+  const props = PropertiesService.getScriptProperties()
+
+  if (props.getProperty("expire_at") && parseInt(props.getProperty("expire_at")) > getTimestamp()) {
+    // access token is valid
+    console.log("access token is valid")
+    return
+  }
+
+  // private client requires BASIC auth header
+  const options = {
+    method : "post",
+    followRedirects: false,
+    contentType: "application/x-www-form-urlencoded",
+    payload : {
+      grant_type: "refresh_token",
+      refresh_token: props.getProperty("refresh_token"),
+      client_id: props.getProperty("client_id")
+    }
+  }
+  // insert BASIC auth if stored client_secret
+  if(props.getProperty("client_secret")) {
+    const encoded_auth = Utilities.base64Encode(props.getProperty("client_id") + ":" + props.getProperty("client_secret"))
+    options.headers = {
+      Authorization : "Basic " + encoded_auth
+    }
+  }
+
+  const response = UrlFetchApp.fetch(url, options);
+  const json = JSON.parse(response);
+
+  props.setProperty("access_token", json.access_token)
+  props.setProperty("refresh_token", json.refresh_token)
+  props.setProperty("expire_at", (getTimestamp() + json.expires_in - 60).toString()) // stored as String
+  // console.log(json)
+
+  console.log("updated access token")
+}
+
+function updateStatus(message) {
+  const url = "https://api.twitter.com/2/tweets"
+
+  const api_params = {
+    text: message
+  }
+
+  refreshToken()
+
+  const props = PropertiesService.getScriptProperties()
+
+  const options = {
+    method : "post",
+    followRedirects: false,
+    headers: {
+      Authorization : "Bearer " + props.getProperty("access_token")
+    },
+    contentType: "application/json",
+    payload : JSON.stringify(api_params)
+  }
+
+  const response = UrlFetchApp.fetch(url, options);
+  const json = JSON.parse(response);
+  console.log(json)
+}
+
+function extractTodayRedords() {
+  const sheet = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId())
+  const env = sheet.getSheetByName("env")
+
+  const data = env.getDataRange().getValues()
+  // extract today's records
+  const today = new Date(Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyy-MM-dd') + 'T00:00:00+09:00')
+  const records = data.filter((row) => row[0] > today)
+  // console.log(records)
+  return records
+}
+
+function highTempDays() {
+  const sheet = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId())
+  const env = sheet.getSheetByName("env")
+
+  const data = env.getDataRange().getValues()
+  // extract cool days
+  const records = data.filter((row) => (row[2] < 25.0) || (row[3] < 25.0)).map((row) => row[0])
+
+  const lastCoolDay = Math.max.apply(null, records)
+  const now = new Date()
+  return Math.floor((now - lastCoolDay) / (1000 * 3600 * 24))
+}
+
+function makeStatusLine(records) {
+  const statuses = []
+  // Make uniq
+  const ids = new Set(records.map((row) => row[5]))
+  ids.forEach((id) => {
+    const rows = records.filter((row) => row[5] === id)
+    const last_row = rows[rows.length - 1]
+    statuses.push("" + last_row[1] + "回->" + last_row[6].toFixed(2) + "V")
+  })
+
+  return statuses.join()
+}
+
+function minTemp() {
+  const records = extractTodayRedords()
+  const rows = records.flatMap((row) => [row[2], row[3]])
+  // const temp = Math.min(...rows)
+  const temp = Math.min.apply(null, rows)
+  // console.log(temp.toFixed(2))
+
+  // get status
+
+  const today = new Date()
+  const date = Utilities.formatDate(today, 'Asia/Tokyo', 'M月d日')
+  let  message = "今朝(" + date + ")の最低室温は、" + temp.toFixed(1) + "℃でした。"
+  const high_temp_days = highTempDays()
+  if (high_temp_days > 2) {
+    // ignore 2 days over 25C
+    message += "連続" + high_temp_days + "日の熱帯夜です。"
+  }
+  message += "\n" + makeStatusLine(records) + "\n#ESP32 #GAS"
+  console.log(message)
+  updateStatus(message)
+}
+
+function maxTemp() {
+  const records = extractTodayRedords()
+  const rows = records.flatMap((row) => [row[2], row[3]])
+  // const temp = Math.max(...rows)
+  const temp = Math.max.apply(null, rows)
+  // console.log(temp.toFixed(2))
+
+  const date = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'M月d日')
+  let message = "今日(" + date + ")の最高室温は、" + temp.toFixed(1) + "℃でした。\n"
+  message += makeStatusLine(records) + "\n#ESP32 #GAS"
+  console.log(message)
+  updateStatus(message)
 }
